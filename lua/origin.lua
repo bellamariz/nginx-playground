f857640abf7b4b4f6735f07d3e7b3c758c8538d3
@bellamariz <isabellafmariz@gmail.com>
2023-08-09 18:59:49 -0300

    Lua module for getting hosts and path for proxy pass - attempt with shared dict

---
+++ lua/origin.lua
@@ -0,0 +1,27 @@
+local cjson = require "cjson"
+local requests = require "resty.requests"
+local origin = {}
+
+local mock_pass = function(){
+  local proxy_pass_data = {
+    ["hosts"] = {"127.0.0.2", "127.0.0.3"},
+    ["paths"] = {
+      ["default"] = "anything-i-want"
+    }
+  } 
+
+  ngx.shared.cached_pass_data:set("destination", cjson.encode(proxy_pass_data))
+}
+
+origin.proxy_pass_data = function(channel){
+  mock_pass()
+  
+  local cached_pass_data = ngx.shared["cached_pass_data"]
+  local destination_data = cached_pass_data:get("destination")
+  local hosts_and_paths = cjson.decode(destination_data)
+  local path = hosts_and_paths["paths"]["default"]
+
+  ngx.ctx.hosts = hosts_and_paths.hosts
+  ngx.ctx.path = hosts_and_paths["paths"]["default"]
+}
+
