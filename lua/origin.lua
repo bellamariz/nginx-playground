1e7ae7b9248879288a9917b616408d40f9b59b4d
@bellamariz <isabellafmariz@gmail.com>
2023-08-10 17:15:04 -0300

    Add new server, rename method and fix Lua sintax

--- lua/origin.lua
+++ lua/origin.lua
@@ -2,18 +2,18 @@ local cjson = require "cjson"
 local requests = require "resty.requests"
 local origin = {}
 
-local mock_pass = function(){
+local mock_pass = function()
   local proxy_pass_data = {
-    ["hosts"] = {"127.0.0.2", "127.0.0.3"},
+    ["hosts"] = {"127.0.0.2", "127.0.0.3", "127.0.0.4"},
     ["paths"] = {
       ["default"] = "anything-i-want"
     }
   } 
 
   ngx.shared.cached_pass_data:set("destination", cjson.encode(proxy_pass_data))
-}
+end
 
-origin.proxy_pass_data = function(channel){
+origin.proxy_pass_data = function(channel)
   mock_pass()
   
   local cached_pass_data = ngx.shared["cached_pass_data"]
@@ -23,5 +23,7 @@ origin.proxy_pass_data = function(channel){
 
   ngx.ctx.hosts = hosts_and_paths.hosts
   ngx.ctx.path = hosts_and_paths["paths"]["default"]
-}
+end
+
+return origin
 
